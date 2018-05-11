b1950fd73e972db619ea1c806eb3fe6bbdb53fd5
jimmy <773792867@qq.com>
2018-05-11 11:55:36 +0800

    cloud

--- tutorial_cloud_configurer/src/main/java/com/cloud/CloudConfigurerStart.java
+++ tutorial_cloud_configurer/src/main/java/com/cloud/CloudConfigurerStart.java
@@ -2,12 +2,13 @@ package com.cloud;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by Jimmy. 2018/5/9  18:18
  */
 @SpringBootApplication
-//@EnableConfigServer
+@EnableConfigServer
 public class CloudConfigurerStart {
 
     public static void main(String[] args) {
