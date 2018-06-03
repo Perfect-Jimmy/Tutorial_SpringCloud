95b8063e568ba133a2d6c87251fa04c5baf62c39
jimmy <773792867@qq.com>
2018-06-03 21:53:29 +0800

    cloud feign

--- tutorial_cloud_book/src/main/java/com/cloud/BookServerStart.java
+++ tutorial_cloud_book/src/main/java/com/cloud/BookServerStart.java
@@ -2,13 +2,14 @@ package com.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by Jimmy. 2018/5/10  17:12
  * 服务提供者 book
  */
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
 public class BookServerStart {
     public static void main(String[] args) {
