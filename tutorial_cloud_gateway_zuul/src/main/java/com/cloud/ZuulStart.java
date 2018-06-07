c106a029fd1e2e1ad545926cf38990d0aa428331
jimmy <773792867@qq.com>
2018-06-07 09:24:54 +0800

    cloud gateway

---
+++ tutorial_cloud_gateway_zuul/src/main/java/com/cloud/ZuulStart.java
@@ -0,0 +1,15 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Jimmy. 2018/6/7  09:23
+ */
+@SpringBootApplication
+public class ZuulStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulStart.class,args);
+    }
+}
